04cb088eea60f24187d8b18d7db6bea2a7c1f3ff
Codermine <scala@codermine.com>
2018-02-06 10:34:51 +0000

    Rimozione warning ALSA

--- Recognition_mic.py
+++ Recognition_mic.py
@@ -3,8 +3,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pocketsphinx.pocketsphinx import *
-from commands import command, notify
+from pocketsphinx import *
+from ctypes import *
+#from commands import command, notify
 import pyaudio
 import subprocess
 import time
@@ -15,10 +16,10 @@ import math
 ###################################### Variables ##################################################################################################################
 
 
-kws_hmm = '/Users/enricogiacalone/Desktop/Enry/chat-bot-pocketsphinx-python-master/pocketsphinx/model/it/it'
-kws_dict = '/Users/enricogiacalone/Desktop/Enry/chat-bot-pocketsphinx-python-master/pocketsphinx/model/it/custom/voxforge_it_sphinx_custom04.dic'
-kws_activation = '/Users/enricogiacalone/Desktop/Enry/chat-bot-pocketsphinx-python-master/custom_keyphrases/keyphrases01.txt'
-kws_commands = '/Users/enricogiacalone/Desktop/Enry/chat-bot-pocketsphinx-python-master/commands.txt'
+kws_hmm = 'pocketsphinx/model/it/it'
+kws_dict = 'pocketsphinx/model/it/custom/voxforge_it_sphinx_custom04.dic'
+kws_activation = 'custom_keyphrases/keyphrases01.txt'
+kws_commands = 'commands.txt'
 
 activation_message = 'Started listening for activation...'
 commands_message = 'Started listening for commands...'
@@ -28,7 +29,25 @@ SHORT_NORMALIZE = (1.0 / 32768.0)
 RATE = 16000
 INPUT_BLOCK_TIME = 1
 INPUT_FRAMES_PER_BLOCK = int(RATE * INPUT_BLOCK_TIME)
+SUPPRESS_ALSA_ERROR = True
 
+###################################### ALSA error handler ###############################################################################################################
+
+# From alsa-lib Git 3fd4ab9be0db7c7430ebd258f2717a976381715d
+# $ grep -rn snd_lib_error_handler_t
+# include/error.h:59:typedef void (*snd_lib_error_handler_t)(const char *file, int line, const char *function, int err, const char *fmt, ...) /* __attribute__ ((format (printf, 5, 6))) */;
+# Define our error handler type
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+def py_error_handler(filename, line, function, err, fmt):
+      #print 'messages are yummy'
+      return
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+asound = cdll.LoadLibrary('libasound.so')
+# Set error handler
+if SUPPRESS_ALSA_ERROR:
+    asound.snd_lib_error_set_handler(c_error_handler)
+else:
+    asound.snd_lib_error_set_handler(NULL)
 
 ###################################### Decode_function ##################################################################################################################
 
@@ -154,10 +173,11 @@ while True:
 
     if audio_a == "okscala" or audio_a == "eiscala":
         
-        
-        audio_file = "/Users/enricogiacalone/Desktop/Enry/chat-bot-pocketsphinx-python-master/suoni-attivazione/Ding.wav"
-        notify('Chat-bot', 'Hey!')
-        return_code = subprocess.call(["afplay", audio_file])
+        print('attivazione avvenuta con successo');
+
+        audio_file = "suoni-attivazione/Ding.wav"
+        #notify('Chat-bot', 'Hey!')
+        #return_code = subprocess.call(["afplay", audio_file])
 
         # audio_c = decode_sphinx(kws_commands, commands_message, quitTimeS=10)
 
@@ -170,8 +190,8 @@ while True:
 
     if noiseLevel > 0.007:
         print("prossima 01: ", noiseLevel)
-        kws_activation = '/Users/enricogiacalone/Desktop/Enry/chat-bot-pocketsphinx-python-master/custom_keyphrases/keyphrases00.txt'
+        kws_activation = 'custom_keyphrases/keyphrases00.txt'
     else:
         print("prossima 00: ", noiseLevel)
-        kws_activation = '/Users/enricogiacalone/Desktop/Enry/chat-bot-pocketsphinx-python-master/custom_keyphrases/keyphrases01.txt'
+        kws_activation = 'custom_keyphrases/keyphrases01.txt'
 
